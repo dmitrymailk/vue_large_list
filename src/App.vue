a17be595ee421e1cbb4159bb7fee1eb228b839c2
dmitrymailk <dmitrymail_k@mail.ru>
2022-04-14 03:18:55 +0300

    create scroller for large lists

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,131 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper" @scroll="scrollEvent" ref="wrapperElem">
+    <Card
+      v-for="(item, index) in centerItems"
+      :key="index"
+      :title="item.title"
+      :text="item.text"
+      :mountedCallback="mountedCallback"
+    />
+    <Card
+      v-for="(item, index) in bottomItems"
+      :key="index"
+      :title="item.title"
+      :text="item.text"
+      :mountedCallback="mountedCallback"
+    />
+  </div>
 </template>
 
+<script>
+import Card from "./components/Card.vue";
+// You can import any component you want as a named export from 'vue-virtualised'. e.g.
+
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    this.allItems = [];
+    return {
+      items: [],
+      upItems: [],
+      centerItems: [],
+      bottomItems: [],
+      currentItem: 0,
+      deltaItem: 100,
+      totalHeight: 0,
+    };
+  },
+  methods: {
+    generateLargeList() {
+      const amount = Math.pow(10, 5);
+      // const amount = 30;
+      let items = [];
+      for (let i = 0; i < amount; i++) {
+        let text = "";
+        const words_amount = this.generateRandom(3, 30);
+        for (let j = 0; j < words_amount; j++) text += " random text";
+        let item_data = {
+          title: `Title ${i}`,
+          text: text,
+        };
+        items.push(item_data);
+      }
+
+      this.allItems = items;
+      this.bottomItems = items.slice(0, this.deltaItem);
+    },
+    generateRandom(min = 0, max = 100) {
+      let difference = max - min;
+      let rand = Math.random();
+      rand = Math.floor(rand * difference);
+      rand = rand + min;
+      return rand;
+    },
+    mountedCallback(height) {
+      this.totalHeight += height;
+    },
+    scrollEvent(e) {
+      // console.log(e);
+      const wrapperElem = this.$refs.wrapperElem;
+      const scrollProgress =
+        wrapperElem.scrollTop /
+        (wrapperElem.scrollHeight - wrapperElem.clientHeight);
+      // console.log(scrollProgress);
+      let overflow = Math.floor(this.allItems.length / this.deltaItem - 1);
+      if (scrollProgress >= 0.98 && this.currentItem < overflow) {
+        console.log("end");
+        this.currentItem += 1;
+        let cur = this.currentItem;
+        this.centerItems = this.bottomItems;
+        this.bottomItems = this.allItems.slice(
+          cur * this.deltaItem,
+          (cur + 1) * this.deltaItem
+        );
+        let centerId = 0;
+        if (this.currentItem == 1) {
+          // console.log(wrapperElem.children.length);
+          centerId = 1;
+        } else centerId = this.deltaItem - 1;
+        console.log(centerId);
+        let lastCenter = wrapperElem.children[centerId];
+        lastCenter.scrollIntoView();
+      } else if (scrollProgress <= 0.01 && this.currentItem > 1) {
+        console.log("start");
+        this.currentItem -= 1;
+        let cur = this.currentItem;
+        this.bottomItems = this.centerItems;
+        this.centerItems = this.allItems.slice(
+          (cur - 1) * this.deltaItem,
+          cur * this.deltaItem
+        );
+
+        let lastCenter = wrapperElem.children[this.deltaItem - 1];
+        lastCenter.scrollIntoView();
+      }
+    },
+  },
+
+  mounted() {
+    console.log("mounted");
+    this.generateLargeList();
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.card {
+  width: 300px;
+  border-radius: 8px;
+  border: 1px solid black;
+  margin-bottom: 10px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.wrapper {
+  overflow-y: scroll;
+  max-height: 300px;
+  width: 501px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <h3>{{ title }}</h3>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text", "title", "mountedCallback"],
+
+  mounted() {
+    // console.log("mounted", this.$el);
+
+    const elementHeight = this.$el.parentNode.offsetHeight + 10;
+    this.$props.mountedCallback(elementHeight);
+  },
+};
+</script>
